81f41e996f4ec7af86be73bcf39424d65b6bbdd5
wannapii <wannapiiii@gmail.com>
2026-08-31 23:33:36 +0300

    Time: 0 ms (100%), Space: 19.4 MB (14.67%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        s = str(num)
+        for i in range(len(s)):
+            if s[i] == '6':
+                res = s[:i] + '9' + s[i+1:]
+                return int(res)
+        return num
+
+
+        
+
